8ba1ceab01ad2e02fc299668c45fc123ea5adb1b
Ochroo <ochroobna033@gmail.com>
2026-09-03 13:28:40 +0800

    Fix ESLint html link errors

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import Link from 'next/link';
 
 export default function News() {
   const [activeNews, setActiveNews] = useState(null)
